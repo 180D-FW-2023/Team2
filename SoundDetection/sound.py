5f832cd6c3b00d912c492d773033f0c012f9e7e2
seaweed2018 <zhenbruin20@g.ucla.edu>
2023-12-06 01:27:37 -0800

    Add existing file

---
+++ SoundDetection/sound.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+from sent import send_simple_email
+from savecsv import append_list_as_row
+import RPi.GPIO as GPIO
+import time
+from datetime import datetime
+import importlib.util
+
+
+
+count = 0
+last_bark_time = None
+
+#GPIO SETUP
+channel = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+def callback(channel):
+        global count, last_bark_time
+        count += 1
+        if (count > 3):
+                print( "Your dog is barking!")
+                #kuanghongchang@outlook.com
+                send_simple_email("zhenbruin20@g.ucla.edu","Woof","Your dog is barking, you might want to check out the live stream?")
+                last_bark_time = datetime.now()
+                append_list_as_row('bark_file.csv', [last_bark_time])
+                count = 0
+        #if GPIO.input(channel):
+                #print( "Your dog barked!")
+                
+        #else:
+                #print( "Your dog barked!")
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=800)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+
+# infinite loop
+while True:
+        time.sleep(1)
