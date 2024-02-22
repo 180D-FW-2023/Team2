f2a2d505f365caa72a214f25f16521ca8f9c8ebe
jessy413 <jessy413@ucla.edu>
2024-02-21 16:52:59 -0800

    renamed gpt

---
+++ ObjectDetection_camera/gptsummarization.py
@@ -0,0 +1,39 @@
+from openai import OpenAI
+from datetime import date
+
+import csv
+
+
+def generate_summary(csv_path):
+
+  # process cvs string
+  object_detect_str = ""
+  with open(csv_path, newline='') as csvfile:
+      spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+      for row in spamreader:
+          object_detect_str += ' '.join(row) + '\n'
+          #print(', '.join(row))
+
+  # opencv request
+
+  client = OpenAI(api_key="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")
+
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {
+        "role": "system",
+        "content": "Summarize content you are provided with in specific time and date in a list. If repetitive movement, merge interval"
+      },
+      {
+        "role": "user",
+        "content": object_detect_str
+      }
+    ],
+    temperature=0.5,
+    max_tokens=200,
+    top_p=1
+  )
+
+  return response.choices[0].message.content
+
